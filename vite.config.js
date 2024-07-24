411343287efac8388764e69c914d7daff32e750c
abhinavmittal554 <abhinavmittal554@gmail.com>
2024-07-24 18:26:50 +0000

    "file add"

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,8 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import fs from 'fs'
+import path from 'path'
+
 
 // https://vitejs.dev/config/
 export default defineConfig({
