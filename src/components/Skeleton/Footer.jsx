1e5f418680a7fcd2abeaf0d244944bf3f3437028
abhinavmittal554 <abhinavmittal554@gmail.com>
2024-07-24 19:02:51 +0000

    config

--- src/components/Skeleton/Footer.jsx
+++ src/components/Skeleton/Footer.jsx
@@ -1,9 +1,5 @@
 import React from "react";
 import "./footer.css";
-import twitter from "../../assets/twitter.svg";
-import github from "../../assets/github.svg";
-import linkedin from "../../assets/linkedin.svg";
-
 export default function Footer() {
   return (
     <footer>
@@ -12,18 +8,21 @@ export default function Footer() {
                 <ul>
                   <li>
                     <a target="_blank" href="https://x.com/abhinav11234" title="Twitter">
-                    <img src={twitter} alt="Twitter" />
+                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="#FAFBFC" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-twitter"><path d="M23 3a10.9 10.9 0 0 1-3.14 1.53 4.48 4.48 0 0 0-7.86 3v1A10.66 10.66 0 0 1 3 4s-4 9 5 13a11.64 11.64 0 0 1-7 2c9 5 20 0 20-11.5a4.5 4.5 0 0 0-.08-.83A7.72 7.72 0 0 0 23 3z"></path></svg>
                     
                     </a>
                   </li>
                   <li>
                     <a target="_blank" href="https://github.com/AbhinavTheDev" title="GitHub">
-                    <img src={github} alt="Github" />
+                    <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-brand-github" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="#FAFBFC" fill="none" stroke-linecap="round" stroke-linejoin="round">
+   <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+   <path d="M9 19c-4.3 1.4 -4.3 -2.5 -6 -3m12 5v-3.5c0 -1 .1 -1.4 -.5 -2c2.8 -.3 5.5 -1.4 5.5 -6a4.6 4.6 0 0 0 -1.3 -3.2a4.2 4.2 0 0 0 -.1 -3.2s-1.1 -.3 -3.5 1.3a12.3 12.3 0 0 0 -6.2 0c-2.4 -1.6 -3.5 -1.3 -3.5 -1.3a4.2 4.2 0 0 0 -.1 3.2a4.6 4.6 0 0 0 -1.3 3.2c0 4.6 2.7 5.7 5.5 6c-.6 .6 -.6 1.2 -.5 2v3.5"></path>
+</svg>
                     </a>
                   </li>
                   <li>
                     <a target="_blank" href="https://www.linkedin.com/in/abhinav76" title="LinkedIn">
-                      <img src={linkedin} alt="LinkedIn" />
+                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="#FAFBFC" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-linkedin"><path d="M16 8a6 6 0 0 1 6 6v7h-4v-7a2 2 0 0 0-2-2 2 2 0 0 0-2 2v7h-4v-7a6 6 0 0 1 6-6z"></path><rect x="2" y="9" width="4" height="12"></rect><circle cx="4" cy="4" r="2"></circle></svg>
                     </a>
                   </li>
                 </ul>
