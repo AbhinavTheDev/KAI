d37227604d1f84b2782e6bb95b5deafc98a69e46
abhinavmittal554 <abhinavmittal554@gmail.com>
2024-07-24 18:25:09 +0000

    add config file

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,19 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    {
+      name: "copy-404",
+      writeBundle() {
+        fs.copyFileSync(
+          path.resolve(__dirname, "404.html"),
+          path.resolve(__dirname, "dist", "404.html")
+        );
+      },
+    },
+  ],
+  base: "/KAI/",
   server: {
     port: 5173,
     host: "0.0.0.0",
